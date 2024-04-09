79ba28ecf1dad75a017009db630d57a619c2fdbc
Beda Vladimir <duracellp@yandex.ru>
2024-04-10 00:28:47 +0300

    Настроен файл для запуска тестов через гитхаб

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,15 @@
 import pytest
 import allure
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @allure.title("Подготовка к тесту. Открытие браузера")
 @pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
